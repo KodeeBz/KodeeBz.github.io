a5990451107a7d32af606f9b242f5d07d19a3600
KodeeBz <154270220+KodeeBz@users.noreply.github.com>
2023-12-27 16:13:31 +0500

    Add files via upload
    test

---
+++ index.tsx
@@ -0,0 +1,199 @@
+
+import React from 'react'
+import cx from 'classnames'
+import styles from './index.module.css'
+
+import { backgroundsStyles } from '@KubiLocal/styles'
+import { ContactUs } from '@KubiLocal/components' // Add this import statement
+
+interface Props {
+  className?: string
+}
+
+
+const Index = ({ className = '' }: Props) => {
+  return (
+    <div className={cx(backgroundsStyles.background, styles.root)}>
+      <div className={styles.frame1}>
+        <div className={styles.rectangle7} />
+
+        <span
+          className={
+            styles.inTheChemistryClassroomSirShaheenAreAPersonificationOfInspirationAndWarmthWithAPassionForTeachingAndAGenuineLoveForHisStudentsHeCreatesAnEngagingAndNurturingEnvironmentThatSparksCuriosityAndFostersGrowthHisEnthusiasmForChemistryIsInfectiousMakingEvenTheMostComplexConceptsSeemApproachableAndExcitingSirShaheenGoesAboveAndBeyondToEnsureEveryStudentUnderstandsTheSubjectOfferingPatientGuidanceAndSupportBeyondAcademicsHeGenuinelyCaresAboutTheWellbeingOfHisStudentsTakingTheTimeToListenAndOfferEncouragementDuringBothSuccessesAndChallengesInTheHeartsOfHisStudentsSirShaheenHoldsASpecialPlaceAsNotOnlyAnEducatorButAlsoAMentorAndFriendLeavingAnIndelibleMarkOnTheirAcademicJourneyWithKindnessAndDedication
+          }
+        >
+          In the chemistry classroom, Sir Shaheen are a
+          personification of inspiration and warmth. With a passion
+          for teaching and a genuine love for his students, he creates
+          an engaging and nurturing environment that sparks curiosity
+          and fosters growth. His enthusiasm for chemistry is
+          infectious, making even the most complex concepts seem
+          approachable and exciting. Sir Shaheen goes above and beyond
+          to ensure every student understands the subject, offering
+          patient guidance and support. Beyond academics, he genuinely
+          cares about the well-being of his students, taking the time
+          to listen and offer encouragement during both successes and
+          challenges. In the hearts of his students, Sir Shaheen holds
+          a special place as not only an educator but also a mentor
+          and friend, leaving an indelible mark on their academic
+          journey with kindness and dedication.
+        </span>
+
+        <div className={styles.ellipse1Wrapper} />
+      </div>
+
+      <div className={styles.rectangle5} />
+
+      <div className={styles.punjabgroupofcolleges1} />
+
+      <ContactUs className={styles.contactUs} /> {/* Use the ContactUs component here */}
+
+      <span className={styles.home}>Home</span>
+
+      <div className={styles.img20221103Wa018001} />
+
+      <span className={styles.eb25}>EB-25</span>
+
+      <span
+        className={
+          styles.inTheHeartwarmingClassroomOfEb25TheWholeClassIsATightknitGroupOfFriendsShiningBrightlyLikeAConstellationOfJoyTheirCamaraderieIsATestamentToTheBeautyOfFriendshipMakingTheClassroomBuzzWithLaughterAndKindnessEveryDayTheyCrackJokesHelpEachOtherWithAssignmentsAndLendAListeningEarBuildingAnUnbreakableBondFromSharingSnacksDuringBreakTimeToCelebratingEachOthersSuccessesTheyCreateAnAtmosphereOfSupportAndEncouragementTheseFriendsHaveBecomeMoreThanJustClassmatesTheyAreASecondFamilyRemindingUsThatTrueFriendshipCanBlossomInUnexpectedPlacesBringingWarmthAndHappinessToEverySharedMoment
+        }
+      >
+        In the heartwarming classroom of EB-25, the whole class is a
+        tight-knit group of friends shining brightly like a
+        constellation of joy. Their camaraderie is a testament to the
+        beauty of friendship, making the classroom buzz with laughter
+        and kindness every day. They crack jokes, help each other with
+        assignments, and lend a listening ear, building an unbreakable
+        bond. From sharing snacks during break time to celebrating
+        each other&#39;s successes, they create an atmosphere of
+        support and encouragement. These friends have become more than
+        just classmates; they are a second family, reminding us that
+        true friendship can blossom in unexpected places, bringing
+        warmth and happiness to every shared moment.
+      </span>
+
+      <span className={styles.introductionToEb25}>
+        Introduction To EB-25:
+      </span>
+
+      <span className={styles.ourTeachers}>Our Teachers</span>
+
+      <span className={styles.ourTeachers1}>
+        Our Class Representatives
+        <br />
+        <br />
+        <br />
+        Repre
+      </span>
+
+      <span className={styles.ourTeachers2}>
+        Gallery:
+        <br />
+        <br />
+        Repre
+      </span>
+
+      <div className={styles.sir}>
+        <span className={styles.classIncharge}>Class Incharge</span>
+
+        <span className={styles.profShaheenShah}>
+          Prof.Shaheen Shah
+        </span>
+      </div>
+
+      <div className={styles.tOpSir} />
+
+      <div className={styles.botoom} />
+
+      <div className={styles.cr} />
+
+      <div className={styles.rectangle11} />
+
+      <div className={styles.untitled2} />
+
+      <span
+        className={
+          styles.onTheLeftMuhammadIbrahimOnTheRightAunMuhammadTwoCheerfulCRsBoundByFriendshipAndKindnessLightUpEveryRoomTheyEnterSpreadingJoyAndWarmthToAllTheirContagiousLaughterAndSupportiveNatureCreateABeautifulHarmonyOfPositivity
+        }
+      >
+        On the left: Muhammad Ibrahim
+        <br />
+        On the right: Aun Muhammad
+        <br /> ... ...
+        <br /> ... ... ...
+        <br />
+        “Two cheerful CRs, bound by friendship and kindness , light up
+        every room they enter, spreading joy and warmth to all. Their
+        contagious laughter and supportive nature create a beautiful
+        harmony of positivity.”
+        <br />
+      </span>
+
+      <div className={styles.v187} />
+
+      <div className={styles.v189} />
+
+      <div className={styles.v192} />
+
+      <span className={styles.asdf}>
+        Left: Principal Sir Shahid Islam <br />
+        Centre: Wamiz Rehman
+        <br />
+        Right: Director PGC Chaudhry Muhammad Akram <br />
+        Wamiz Rehman recieving Rs.2000 for participating in daily
+        college surveys.
+      </span>
+
+      <span className={styles.asdf1}>
+        Left: Unknown
+        <br />
+        Centre Left: Sohaib
+        <br />
+        Centre Right: Muhammad Ubaid
+        <br />
+        Right: Sir Shahid Islam <br />
+        Ubaid and Sohaib receiving certificates and cash award for
+        winning the Science Quiz
+      </span>
+
+      <span
+        className={
+          styles.leftPrincipalSirShahidIslamCentreWamizRehmanRightDirectorPgcChaudhryMuhammadAkramWamizRehmanRecievingRs2000ForParticipatingInDailyCollegeSurveys
+        }
+      >
+        Left:Mustafa aka Mulla
+        <br />
+        Right:Adan
+        <br /> “Qari saab apne agle shikar ke sath”
+      </span>
+
+      <div className={styles.img20230119Wa00491} />
+
+      <span
+        className={
+          styles.sirShaheenWithStudentsOfEb25InclOneImposterOnATripToMurree
+        }
+      >
+        Sir Shaheen with students of EB-25 (incl. one imposter) on a
+        trip to Murree
+      </span>
+
+      <div className={styles.rectangle51} />
+
+      <div className={styles.punjabgroupofcolleges11} />
+
+      <span className={styles.aWebsiteToRepresentEb25}>
+        A website to represent EB-25
+      </span>
+
+      <span className={styles.madeByEb25ForEb25}>
+        Made by EB-25 for EB25
+      </span>
+
+      <span className={styles.madeByAticG}>Made by: Atic G.</span>
+    </div>
+  )
+}
+
+export default Index
